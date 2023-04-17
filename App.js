42cabd86472f97d75e88eb06e2e338639dbf99d7
Sandra <sandra.kawakame@auaha.net>
2023-04-17 07:20:45 -0300

    app biscoito da sorte 100%

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
 export default function App() {
+  const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textofrase, setTextoFrase] = useState('')
+
+  let frases = [
+    'Siga os bons e aprenda com eles.',
+    'O bom-senso vale mais do que muito conhecimento.',
+    'O riso é a menor distância entre duas pessoas.',
+    'Deixe de lado as preocupações e seja feliz.',
+    'Realize o óbvio, pense no improvável e conquiste o impossível.',
+    'Acredite  em milagres, mas não dependa deles.',
+    'A maior barreira para o sucesso é o medo do fracasso.'
+  ]
+
+  function quebraBiscoito(){
+    //Math.floor - pega o numero inteiro
+    let numeroAleatorio = Math.floor(Math.random() * frases.length)
+    setTextoFrase(' " ' + frases[numeroAleatorio] + ' " ')
+    setImg(require('./src/biscoitoAberto.png'))
+  }
+
+  function reiniciar(){
+    setImg(require('./src/biscoito.png'));
+    setTextoFrase('');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image 
+        source={img}
+        style={styles.img}
+      />
+
+      <Text style={styles.textofrase}>{textofrase}</Text>
+
+      <TouchableOpacity style={styles.botao} onPress={ quebraBiscoito }>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.botao, {marginTop: 15, borderColor: '#121212'}]} onPress={ reiniciar }>
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, {color:'#121212'}]}>Reiniciar biscoito</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +59,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: 230,
+    height: 230,
+  },
+  textofrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  botao:{
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25,
+
+  },
+  btnArea:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnTexto:{
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22',
+  }
+
 });
---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from "react-native/types";
+import App from "./App";
+import { nome as biscoitoSorte } from './app;json'
+
+AppRegistry.registerComponent(biscoitoSorte, () => App)
